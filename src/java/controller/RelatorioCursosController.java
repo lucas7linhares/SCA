f6b7e5f4db0d1f88987703290a859a6a78f9786e
Lucas Linhares <Lucas Linhares@10.240.7.165>
2018-05-17 21:30:48 -0300

    Removida biblioteca não utilizada.

--- src/java/controller/RelatorioCursosController.java
+++ src/java/controller/RelatorioCursosController.java
@@ -18,7 +18,6 @@ import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.view.JasperViewer;
 
 /**
  *
